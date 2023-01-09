6f2d08a5c85360de9e8d753dca6cf5fd6456ca13
tomryan01 <tomryan01@outlook.com>
2023-01-09 15:49:32 +0000

    use sgd for least-squares sampler

--- sampling/least_squares_sampling.py
+++ sampling/least_squares_sampling.py
@@ -1,14 +1,11 @@
 import numpy as np
 from scipy.optimize import minimize
 from tqdm import tqdm
-from helper_functions import mat_norm
+from helper_functions import mat_norm, subsample
 from models.regression_model import RegressionModel
 
-#-- ### NOTE ###
-#-- As of now this class will not work, it is not fully implemented
-
 class LeastSquaresSampler():
-	def __init__(self, model, sampler, mean):
+	def __init__(self, model, sampler, mean, batch_size=None):
 		### Ensure Model is Correct
 		assert isinstance(model, RegressionModel)
 		self.model = model
@@ -24,11 +21,13 @@ class LeastSquaresSampler():
 		test_epsilon = np.ones(self.model.nm)
 		test_theta_0 = np.ones(self.model.d_dash)
 		test_A = np.diag(np.ones(self.model.d_dash))
-		# TODO: Mock subsamples of B and Phi to test sampling function
-		# 	Presumably the batch size for SGD must be known and universal so that it can be checked that the sampling
-		# 	function accepts this n' and not the larger n
+
+		# Mock subsamples of Phi and B to test format of sampler
+		idx = subsample(self.model.n, self.model.n/batch_size)
+		Phi = self.model.generate_Phi(self.model.X, self.model.phi, idx)
+		B = self.model.generate_B(self.model.B_i, idx)
 		try:
-			out = sampler(test_z, test_epsilon, test_theta_0, self.model.B, test_A, self.model.Phi)
+			out = sampler(test_z, test_epsilon, test_theta_0, B, test_A, Phi)
 		except:
 			raise Exception("Malformed sampling function, should take arguments: z, epsilon, theta_0, B, A, Phi")
 
@@ -38,9 +37,14 @@ class LeastSquaresSampler():
 		self.sampler = sampler
 
 		### Ensure mean is of correct dimensions
-		# NOTE: Here the mean is likely to be computed using MC for a high-dimensional problem
 		assert mean.shape[0] == self.model.d_dash, "Length of mean should be equal to the number of parameters {}, got {}".format(self.model.d_dash, mean.shape[0])
 		self.mean = mean
+
+		### Setup batch size
+		if not batch_size:
+			self.batch_size = self.model.n
+		else:
+			self.batch_size = batch_size
 
 	def get_Y(self):
 		return self.model.Y
@@ -51,7 +55,7 @@ class LeastSquaresSampler():
 	def get_d_dash(self):
 		return self.model.d_dash
 
-	def E_step(self, num_samples):
+	def E_step(self, num_samples, Phi, B, add_mean=True):
 		samples = np.zeros((num_samples, self.model.d_dash))
 		for i in range(num_samples):
 			epsilon = np.zeros(self.model.nm)
@@ -61,14 +65,16 @@ class LeastSquaresSampler():
 				else:
 					epsilon[j*self.model.m:(j+1)*self.model.m] = np.random.multivariate_normal(np.zeros(self.model.m), np.linalg.inv(np.diag(self.model.B_i[j])))
 			theta_0 = np.random.multivariate_normal(np.zeros(self.model.d_dash), np.linalg.inv(self.model.A))
-			# TODO: Explore the use of different minimisation methods, i.e. SGD
-			samples[i] = minimize(self.sampler, x0 = np.zeros(self.model.d_dash), method='Nelder-Mead', args = (epsilon, theta_0, self.model.B, self.model.A, self.model.Phi)).x
-		return samples
+			samples[i] = minimize(self.sampler, x0 = np.zeros(self.model.d_dash), method='Nelder-Mead', args = (epsilon, theta_0, B, self.model.A, Phi)).x
+		if not add_mean:
+			return samples
+		return samples + self.mean
 
-	def M_step(self, samples):
+	def M_step(self, samples, Phi, B):
+		M = Phi.T @ B @ Phi
 		gamma = 0
 		for s in samples[:]:
-			gamma += s.T @ self.model.M @ s
+			gamma += s.T @ M @ s
 		gamma /= samples.shape[0]
 		return np.diag(np.full(self.model.d_dash, gamma / np.linalg.norm(self.mean)**2))
 
@@ -79,11 +85,14 @@ class LeastSquaresSampler():
 		A_mean = np.zeros(max_iters)
 		A = np.diag(np.full(self.model.d_dash, alpha_0))
 		for i in tqdm(range(max_iters)):
+			# compute Phi using a minibatch
+			idx = subsample(self.model.n, self.model.n/self.batch_size)
+			Phi = self.model.generate_Phi(self.model.X, self.model.phi, idx)
+			B = self.model.generate_B(self.model.B_i, idx)
 			A_mean[i] = np.linalg.norm(A)
-			samples = self.E_step(int_samples)
-			A = self.M_step(samples)
-		# TODO: this will return a zero-mean result, need to determine how to include mean for SGD
-		return A_mean, self.E_step(num_samples)
+			samples = self.E_step(int_samples, Phi, B)
+			A = self.M_step(samples, Phi, B)
+		return A_mean, self.E_step(num_samples, Phi, B, add_mean=True)
 
 def minimizer1(z, epsilon, theta_0, B, A, Phi):
 	a = 0.5*mat_norm(B, Phi @ z - epsilon)
